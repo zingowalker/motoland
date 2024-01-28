ac75fe29c669cea847856cad13e32da7c5e41adb
Tsetan Dorje <tsetangrace@gmail.com>
2024-01-28 11:28:20 +0530

    edit

--- app/rent-motorcycle/page.tsx
+++ app/rent-motorcycle/page.tsx
@@ -18,7 +18,7 @@ export default async function page({ searchParams }: PageProps) {
     allMotorcycles.length < 1 ||
     !allMotorcycles;
   return (
-    <div className="mt-12 xl:py-12 py-10 px-10 " id="discover">
+    <div className="mt-12 xl:py-12 py-10 px-10" id="discover">
       <div className="md:items-center flex flex-col gap-y-2.5">
         <h1 className="text-4xl font-bold">Motorcycle Catalogue</h1>
         <p>Explore motorcycle you might like</p>
@@ -37,7 +37,7 @@ export default async function page({ searchParams }: PageProps) {
         <section className="px-16">
           <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-14">
             {allMotorcycles?.map((motorcycle) => (
-              <MotorcycleCard motorcycle={motorcycle} />
+              <MotorcycleCard key={motorcycle.model} motorcycle={motorcycle} />
             ))}
           </div>
 
