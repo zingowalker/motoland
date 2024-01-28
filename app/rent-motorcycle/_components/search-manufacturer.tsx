deca7eb46c4539a3c6fcbb8ca0f8a53f0a7dc742
Tsetan Dorje <tsetangrace@gmail.com>
2024-01-28 11:45:32 +0530

    removed query

--- app/rent-motorcycle/_components/search-manufacturer.tsx
+++ app/rent-motorcycle/_components/search-manufacturer.tsx
@@ -43,11 +43,11 @@ const SearchManufacturer = ({
           />
 
           <Transition
-            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            as={Fragment}
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+            afterLeave={() => setQuery("")}
           >
             <Combobox.Options
               className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
@@ -58,7 +58,7 @@ const SearchManufacturer = ({
                   value={query}
                   className="cursor-default select-none py-2 pl-10 pr-4"
                 >
-                  Create {query}
+                  Create
                 </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
@@ -81,7 +81,6 @@ const SearchManufacturer = ({
                           {item}
                         </span>
 
-                        {/* Show an active blue background color if the option is selected */}
                         {selected ? (
                           <span
                             className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
